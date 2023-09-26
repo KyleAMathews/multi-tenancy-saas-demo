e22530213d4e4d447ad4a00afd02224c5930f69f
Kyle Mathews <mathews.kyle@gmail.com>
2023-09-26 14:03:17 -0700

    Remove a bunch of unused code

--- app-todos/app/root.tsx
+++ app-todos/app/root.tsx
@@ -10,17 +10,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return json({ user: await getUser(request) });
-};
 
 export default function App() {
   return (
