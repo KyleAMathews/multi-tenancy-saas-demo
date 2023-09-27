2016d317af27236ac1862a44d7dc72d8918e8f78
Kyle Mathews <mathews.kyle@gmail.com>
2023-09-27 10:03:48 -0700

    Remove unneeded files

--- server/get-db.ts
+++
@@ -1,9 +0,0 @@
-import path from "path"
-import { createClient } from "@libsql/client"
-
-export function getDb(baseDir, dbName) {
-  const dbPath = path.join(baseDir, `${dbName}.db`)
-  const db = createClient({ url: `file:${dbPath}` })
-
-  return db
-}
